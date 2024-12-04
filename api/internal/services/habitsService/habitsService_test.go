341d5f7363ebc2aa5fc8f9a99849ffee6de543e0
Reid Mason <maddogshain132@gmail.com>
2024-12-04 01:23:41 +0000

    feat: added unit test for creating habit

--- api/internal/services/habitsService/habitsService_test.go
+++ api/internal/services/habitsService/habitsService_test.go
@@ -23,8 +23,14 @@ func (m mockHabitStorage) UpdateHabit(_ context.Context, _ sqlite3Storage.Update
 	return sqlite3Storage.Habit{}, nil
 }
 
-func (m mockHabitStorage) CreateHabit(_ context.Context, _ sqlite3Storage.CreateHabitParams) (sqlite3Storage.Habit, error) {
-	return sqlite3Storage.Habit{}, nil
+func (m mockHabitStorage) CreateHabit(_ context.Context, habit sqlite3Storage.CreateHabitParams) (sqlite3Storage.Habit, error) {
+	return sqlite3Storage.Habit{
+		ID:     2,
+		Name:   habit.Name,
+		Colour: habit.Colour,
+		Active: true,
+		Index:  habit.Index,
+	}, nil
 }
 
 func (m mockHabitStorage) DeleteHabit(_ context.Context, _ int64) (sqlite3Storage.Habit, error) {
@@ -83,6 +89,60 @@ func TestGetActiveHabits(t *testing.T) {
 				expectedHabit := tc.expectedHabits[i]
 				assert.Equal(t, habit, expectedHabit, "expected habit %v but got %v", expectedHabit, habit)
 			}
+		})
+	}
+}
+
+func TestCreateHabit(t *testing.T) {
+	tests := []struct {
+		newHabitName   string
+		newHabitColour string
+		newHabitId     int64
+		name           string
+		habits         []sqlite3Storage.Habit
+		expectedHabit  Habit
+	}{
+		{
+			name:           "creates a habit",
+			newHabitName:   " Habit 2 ",
+			newHabitColour: " #ffffff ",
+			newHabitId:     1,
+			expectedHabit: Habit{
+				Id:     2,
+				Name:   "Habit 2",
+				Colour: "#ffffff",
+				Active: true,
+				Index:  2,
+			},
+			habits: []sqlite3Storage.Habit{
+				{
+					ID:     1,
+					Name:   "Habit 1",
+					Colour: "#000000",
+					Active: true,
+					Index:  1,
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			storage := mockHabitStorage{
+				habits: tc.habits,
+			}
+			service := NewHabitService(storage, &logger.MockLogger{}, &mockHabitEntryStorage{})
+
+			// Act
+			habit, err := service.CreateHabit(tc.newHabitId, tc.newHabitName, tc.newHabitColour)
+
+			// Assert
+			if err != nil {
+				t.Errorf("expected no error but got: %v", err)
+			}
+
+			assert.Equal(t, habit, tc.expectedHabit, "expected habit %v but got %v", tc.expectedHabit, habit)
 		})
 	}
 }
