bac1f4599ba2493f4e45b8267f1924b8cfead193
Reid Mason <maddogshain132@gmail.com>
2024-12-04 01:02:37 +0000

    feat: added unit tests for getting active habits

--- api/go.mod
+++ api/go.mod
@@ -7,12 +7,14 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.23
 	github.com/pressly/goose/v3 v3.22.1
 	github.com/rs/cors v1.11.1
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/text v0.18.0
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -20,10 +22,12 @@ require (
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ api/internal/services/habitsService/habitsService_test.go
@@ -0,0 +1,88 @@
+package habitsService
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ReidMason/habit-tracker/internal/logger"
+	"github.com/ReidMason/habit-tracker/internal/services/models"
+	sqlite3Storage "github.com/ReidMason/habit-tracker/internal/storage/database/sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockHabitStorage struct {
+	err    error
+	habits []sqlite3Storage.Habit
+}
+
+func (m mockHabitStorage) GetHabits(_ context.Context, _ int64) ([]sqlite3Storage.Habit, error) {
+	return m.habits, m.err
+}
+
+func (m mockHabitStorage) UpdateHabit(_ context.Context, _ sqlite3Storage.UpdateHabitParams) (sqlite3Storage.Habit, error) {
+	return sqlite3Storage.Habit{}, nil
+}
+
+func (m mockHabitStorage) CreateHabit(_ context.Context, _ sqlite3Storage.CreateHabitParams) (sqlite3Storage.Habit, error) {
+	return sqlite3Storage.Habit{}, nil
+}
+
+func (m mockHabitStorage) DeleteHabit(_ context.Context, _ int64) (sqlite3Storage.Habit, error) {
+	return sqlite3Storage.Habit{}, nil
+}
+
+type mockHabitEntryStore struct{}
+
+type mockHabitEntryStorage struct{}
+
+func (m mockHabitEntryStorage) GetHabitEntries(id int64) ([]models.HabitEntry, error) {
+	return nil, nil
+}
+
+func TestGetActiveHabits(t *testing.T) {
+	tests := []struct {
+		name           string
+		habits         []sqlite3Storage.Habit
+		expectedHabits []Habit
+	}{
+		{
+			name: "returns only active habits",
+			habits: []sqlite3Storage.Habit{
+				{ID: 1, Name: "Habit 1", Active: true},
+				{ID: 2, Name: "Habit 2", Active: false},
+				{ID: 3, Name: "Habit 3", Active: true},
+			},
+			expectedHabits: []Habit{
+				{Id: 1, Name: "Habit 1", Active: true},
+				{Id: 3, Name: "Habit 3", Active: true},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			storage := mockHabitStorage{
+				habits: tc.habits,
+			}
+			service := NewHabitService(storage, &logger.MockLogger{}, &mockHabitEntryStorage{})
+
+			// Act
+			habits, err := service.GetActiveHabits(1)
+
+			// Assert
+			if err != nil {
+				t.Errorf("expected no error but got: %v", err)
+			}
+
+			if len(habits) != len(tc.expectedHabits) {
+				t.Errorf("expected %d habits but got %d", len(tc.expectedHabits), len(habits))
+			}
+
+			for i, habit := range habits {
+				expectedHabit := tc.expectedHabits[i]
+				assert.Equal(t, habit, expectedHabit, "expected habit %v but got %v", expectedHabit, habit)
+			}
+		})
+	}
+}
